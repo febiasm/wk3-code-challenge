96959f193e01b066d66a2790293d48d50e0f7b19
febiasm <trevorfebias@gmail.com>
2022-12-13 08:49:32 +0300

    done

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # wk3-code-challenge
+
+## Description 
+This is an aplication where a use can view a list of movies and buy movie tickets online 
+
+This aplication uses JavaScript script `fetch()` method to retrieve data from a remote `URL`, which is then parsed as `JSON` and used to dynamically generate a series of elements on a web page. The elements are `appended` to a container element, which is selected using the `querySelector()` method.
+
+When the `fetch()` method is called, it sends a request to the specified `URL` and returns a `Promise` object. When the `Promise` is resolved, the `then()` method is called, which takes a callback function as an argument. This callback function is passed the `response` object, which is then converted to `JSON` using the `json()` method.
+
+Once the data has been parsed as `JSON`, it is used to generate a series of elements on the page. For each film in the films array, a div element is created and assigned various properties, such as a src attribute for an img element and various innerHTML values for other elements. These elements are then appended to the container element on the page.
+
+## Running live Hosted Application.
+
+Click on this link to run the application.
+
+    https://wk3-code-challenge.vercel.app/
+
+This web page consists of a list of movies each with a different show time and limited number of tickets.
+
+A user can buy a ticket for an intrested movie by clicking on the buy ticket button 
+
+In the event that all tickets are sold out and a user tryes to buy a ticket the user  will be alerted that all tickets have been sold out. 
+
+## cloning the application.
+Open your terminal and navigate to where you'd like to download the project. Clone the repo by using the following:
+
+    git clone https://github.com/febiasm/wk3-code-challenge
+
+Or paste in the copied SSH link from GitHub It should look something like this:
+
+    git clone git@github.com:febiasm/wk3-code-challenge.git
+
+upon completion, a new folder with the GitHub name of the project will be present. Change directory into this folder to access the project files.
+
+    cd wk3-code-challenge
+
+Open it in Visual Studio Code
+
+    code .
+
+congratulations you have successfully cloned the app.
+
+## author :
+This project was contributed by Trevor Febias
+## License
+This project is licensed by `ISC`
---
+++ javaScript/index.js
@@ -0,0 +1,112 @@
+window.onload = () => {
+    // Show the loading screen
+    document.getElementById("loading-screen").style.display = "block";
+  
+    // Hide the loading screen after all content has loaded
+    setTimeout(() => {
+      document.getElementById("loading-screen").style.display = "none";
+    }, 6000); // 6000 milliseconds = 6 second
+  }
+
+
+const URL = "https://api.npoint.io/d6235e8798df3b64dc3f"
+
+
+// Send a GET request to the specified URL and return a promise
+fetch(URL)
+  .then(response => response.json())
+
+  // Convert the response to a JavaScript object
+  .then(data => {
+   const films = data.films;
+
+       // Get the container element from the HTML page
+    const container = document.querySelector('#myContainer');
+
+     // Loop over the films array and create a card for each film
+    films.forEach(film => {
+        const tittle= film.title
+        const poster= film.poster
+        const description= film.description
+        const runtime = film.runtime
+        const showtime = film.showtime
+        const capacity = film.capacity
+        const ticketsSold = film.tickets_sold
+
+        // Calculate the number of available tickets
+        let Availabletickets =  capacity - ticketsSold
+       
+        // Create a div element with the card class
+      const card = document.createElement('div');
+      card.className = 'card';
+      card.style.width = '18rem';
+      card.style.margin = '14px'
+      
+        // Create an img element with the card-img-top class and set the src attribute to the film's poster
+      const img = document.createElement('img');
+      img.src = poster;
+      img.className = 'card-img-top';
+      card.appendChild(img);
+
+       // Create a div element with the card-body class
+        // Create an h4 element with the card-title class and set the innerHTML to the film's title
+      const cardBody = document.createElement('div');
+      cardBody.className = 'card-body';
+      const h4 = document.createElement('h4');
+      h4.className = 'card-title';
+      h4.innerHTML = tittle;
+      cardBody.appendChild(h4);
+
+      // Create a p element with the card-text class and set the innerHTML to the film's description
+      const p = document.createElement('p');
+      p.className = 'card-text';
+      p.innerHTML = description;
+      cardBody.appendChild(p);
+
+
+      // Create an h6 element with the runTime class and set the innerHTML to the film's runtime
+      const h6 = document.createElement('h6');
+      h6.className = 'runTime';
+      h6.innerHTML = `Run Time: ${runtime} Minutes`;
+      cardBody.appendChild(h6);
+      
+
+      // Create an h6 element with the runTime class and set the innerHTML to the film's showtime
+      const ShowTime = document.createElement('h6');
+      ShowTime.className = 'showTime';
+      ShowTime.innerHTML = `Show Time: ${showtime}`;
+      cardBody.appendChild(ShowTime);
+
+        // Create an h6 element with the available-tickets class and set the innerHTML to the film's Availabletickets
+      const AvailableTickets = document.createElement('h6');
+      AvailableTickets.className = 'available-tickets';
+      AvailableTickets.innerHTML = `Available Tickets: ${Availabletickets}`;
+      cardBody.appendChild(AvailableTickets);
+
+      // creat an a element with the btn btn-success class and set innerHTML to buy ticket .
+      const button = document.createElement('a');
+      button.className = 'btn btn-success';
+      button.innerHTML = 'Buy Ticket';
+      cardBody.appendChild(button);
+      card.appendChild(cardBody);
+      container.appendChild(card);
+
+      // event listener for button 
+      button.addEventListener('click', () => {
+        if (Availabletickets >0){
+            Availabletickets -= 1;}
+        else{
+            button.className = 'btn btn-danger';
+            button.innerHTML = 'Sold Out';
+            alert ("Sold Out")
+           
+
+            
+        }
+        AvailableTickets.innerHTML = `Available Tickets: ${Availabletickets}`;
+        
+    });
+
+      
+    
+    })})
